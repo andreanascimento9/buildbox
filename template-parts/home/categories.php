02759eb9498014ed7d201107bd9e6cc95359faaa
andreadn <andre.d.nascimento@uol.com.br>
2023-01-27 17:10:00 -0300

    categories in home

--- template-parts/home/categories.php
+++ template-parts/home/categories.php
@@ -1,109 +1,49 @@
+<?php
+
+$args = [
+    'post_type' => 'videos',
+    'post_status' => 'publish',
+    'category_name' => 'filmes',
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'posts_per_age' => -1
+];
+
+$films = new WP_Query($args);
+
+?>
+
+
 <section class="categories">
-    <div class="container">
-        <h2>Filmes</h2>
 
-        <div class="list-items">
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">130m</div>
-                <div class="title">Proin sollicitudi</div>
+    <?php
+    if ($films->have_posts()) :
+        while ($films->have_posts()) : $films->the_post();
+            $film_id = get_the_ID();
+            $categories_films = get_the_category($film_id);
+    ?>
+            <div class="container">
+                <?php foreach ($categories_films as $category_film) : ?>
+                    <?php if ($category_film->slug !== 'destaque') : ?>
+                        <h2><?php echo $category_film->name; ?></h2>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+
+                <div class="list-items">
+                    <div class="item">
+                        <div class="thumb" style="background: url(<?php echo get_the_post_thumbnail_url() ?>"></div>
+                        <div class="info"><?php echo get_post_meta($film_id, 'tempo_video_min', true) ?>m</div>
+                        <div class="title"><?php echo get_the_title() ?></div>
+                    </div>
+
+
+                </div>
             </div>
 
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
+    <?php
+        endwhile;
+    endif;
+    wp_reset_postdata();
+    ?>
 
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container">
-        <h2>Filmes</h2>
-
-        <div class="list-items">
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">130m</div>
-                <div class="title">Proin sollicitudi</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-
-            <div class="item">
-                <div class="thumb" style="background: url(<?php echo get_template_directory_uri() ?>/arranha-ceus.jpg)"></div>
-                <div class="info">160m</div>
-                <div class="title">Maecenas tempus mi a ex malesuada</div>
-            </div>
-        </div>
-    </div>
 </section>
